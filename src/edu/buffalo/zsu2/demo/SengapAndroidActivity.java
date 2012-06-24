3a023e2c75419c6c24b4630fcf65fb1b3138d1b2
Suzker <szhiliang@gmail.com>
2012-06-25 04:51:33 +0800

    Android Manifest file edited for running phonegap, main activity edited, hello world created

--- src/edu/buffalo/zsu2/demo/SengapAndroidActivity.java
+++ src/edu/buffalo/zsu2/demo/SengapAndroidActivity.java
@@ -1,13 +1,13 @@
 package edu.buffalo.zsu2.demo;
 
-import android.app.Activity;
+import org.apache.cordova.*;
 import android.os.Bundle;
 
-public class SengapAndroidActivity extends Activity {
+public class SengapAndroidActivity extends DroidGap{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
